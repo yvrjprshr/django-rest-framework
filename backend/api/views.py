a608904add417a84c5f229e8a0689f3ac64880d0
unknown <yuvrajparashar710@gmail.com>
2022-10-27 11:38:40 +0530

    oct_27

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,17 @@
-from django.http import JsonResponse
+import json
+from django.http import JsonResponse, request
 
 # Create your views here.
-def api_home(requests, *args, **kwargs):
+def api_home(request, *args, **kwargs):
+    body=request.body
+    data={}
+    try:
+        data=json.loads(body)
+    except:
+        pass
+    print("++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+    print('reached here')
+    print(data.keys())
+    print(request.headers)
+    print("++++++++++++++++++++++++++++++++++++++++++++++++++++++")
     return JsonResponse({"message":"Hi There! This is your Django API Home"})
